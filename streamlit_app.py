e1a028a8220d15362ababb3bf6d89f7296873481
mattapally.sainithin <71919295+Mattapallysainithin@users.noreply.github.com>
2025-03-21 15:02:05 -0500

    Update streamlit_app.py
    connected changes needed bc opf move from sis to snis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -12,7 +12,8 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-
+cnx= st.connection("snowflake")
+session=cnx.session()
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe.to_pandas()['FRUIT_NAME'].tolist())
 
 if ingredients_list:
